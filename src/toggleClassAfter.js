172fe125dc9ba8e02a59a54184561c7eaed96f18
Robert F. Popeleski <robpop@users.noreply.github.com>
2019-09-21 23:43:54 -0400

    Fixed tabbing issues

--- src/toggleClassAfter.js
+++ src/toggleClassAfter.js
@@ -9,46 +9,46 @@
       value = undefined;
     }
     var
-			self = this,
-			timeouts = [],
-			intervals = [],
+	self = this,
+	timeouts = [],
+	intervals = [],
 
-			// Make a call to toggleClass if no additional timeouts or intervals are present
-			callback = function() {
-				window.setTimeout( function() {
-					if ( timeouts.length <= 0 && intervals.length <= 0 ) {
-						return jQuery( self ).toggleClass( value, stateVal );
-					}
-				}, 0 );
-			},
+	// Make a call to toggleClass if no additional timeouts or intervals are present
+	callback = function() {
+		window.setTimeout( function() {
+			if ( timeouts.length <= 0 && intervals.length <= 0 ) {
+				return jQuery( self ).toggleClass( value, stateVal );
+			}
+		}, 0 );
+	},
 
-			// Proxy setTimeout, setInterval, clearTimeout, and clearInterval for internal use
-			timeout = function( func, duration ) {
-				timeouts.push(
-					window.setTimeout( function() {
-						timeouts.shift();
-						callback();
-					}, duration )
-				);
-				return window.setTimeout( func, duration );
-			},
-			interval = function( func, duration ) {
-				intervals.push(
-					window.setInterval( func, duration )
-				);
-				return intervals[ intervals.length - 1 ];
-			},
-			cinterval = function( id ) {
-				intervals.splice( intervals.indexOf( intervals[ id ] ), 1 );
-				window.clearInterval( id );
+	// Proxy setTimeout, setInterval, clearTimeout, and clearInterval for internal use
+	timeout = function( func, duration ) {
+		timeouts.push(
+			window.setTimeout( function() {
+				timeouts.shift();
 				callback();
-			},
-			ctimeout = function( id ) {
-				window.clearTimeout( timeouts.indexOf( timeouts[ id ] ) );
-				timeouts.splice( timeouts.indexOf( timeouts[ id ] ), 1 );
-				window.clearTimeout( id );
-				callback();
-			};
+			}, duration )
+		);
+		return window.setTimeout( func, duration );
+	},
+	interval = function( func, duration ) {
+		intervals.push(
+			window.setInterval( func, duration )
+		);
+		return intervals[ intervals.length - 1 ];
+	},
+	cinterval = function( id ) {
+		intervals.splice( intervals.indexOf( intervals[ id ] ), 1 );
+		window.clearInterval( id );
+		callback();
+	},
+	ctimeout = function( id ) {
+		window.clearTimeout( timeouts.indexOf( timeouts[ id ] ) );
+		timeouts.splice( timeouts.indexOf( timeouts[ id ] ), 1 );
+		window.clearTimeout( id );
+		callback();
+	};
     func.call( null, timeout, ctimeout, interval, cinterval );
     callback();
   };
